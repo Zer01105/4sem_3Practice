57f14a52d72fd852ae5d8b2d2e9987587ed7c8b3
zer0 <zer0@gmail.com>
2025-05-20 17:48:02 +0300

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+function Footer(){
+    return(
+        <footer className = "page-footer teal darken-2">
+          <div className = "footer-copyright">
+            <div className = "container">
+            © выполнил Шмаров Андрей студент группы ТКБО-01-23
+            <a className = "grey-text text-lighten-4 right" href="https://github.com/Zer01105/4sem_3Practice.git">GitHub Repository</a>
+            </div>
+          </div>
+        </footer>
+    )
+}
+
+export {Footer};
---
+++ src/components/preloader.jsx
@@ -0,0 +1,9 @@
+function Preloader(){
+    return(
+        <div className="progress skyblue lighten-4">
+            <div className="indeterminate skyblue darken-2"></div>
+        </div>
+    )
+}
+
+export {Preloader};
